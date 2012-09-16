47b07e5f2868157846b964764e08b53438da7a1a
DarrenN <info@v25media.com>
2012-09-16 14:23:44 -0400

    removed superfluous code from config.rb

--- config.rb
+++ config.rb
@@ -38,52 +38,6 @@ ready do
   end
 end
 
-### 
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-
-#   config.output_style    = :compact
-#   config.sass_dir        = "sass"
-#   config.css_dir         = "css"
-#   config.images_dir      = "img"
-#   config.javascripts_dir = "js"
-# end
-
-###
-# Haml
-###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Page command
-###
-
-# Per-page layout changes:
-# 
-# With no layout
-# page "/path/to/file.html", :layout => false
-# 
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-# 
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
 set :markdown_engine, :kramdown
 set :markdown, :layout_engine => :erb, :tables => true, :autolink => true
 
@@ -98,11 +52,9 @@ page "/atom.json", :proxy => "/json_articles.json", :layout => false, :ignore =>
   @atom_article = ''
 end
 
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+###
+# Code Highlighting
+###
 
 use Rack::Codehighlighter, 
   :pygments_api,
@@ -145,24 +97,5 @@ set :images_dir, "img"
 # Build-specific configuration
 configure :build do
   activate :minify_css
-  #activate :gzip
-  #activate :cache_buster
   activate :asset_hash
-  
-  # Minify Javascript on build
-  # activate :minify_javascript
-  
-  # Enable cache buster
-  # activate :cache_buster
-  
-  # Use relative URLs
-  # activate :relative_assets
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end
